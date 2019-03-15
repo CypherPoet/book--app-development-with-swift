25301cc6fbe61675760b4d8975625673179b513c
CypherPoet <CypherPoet@gmail.com>
2019-03-15 14:44:21 -0400

    complete 2-1 lab

---
+++ 2 - Introduction to UIKit/Labs/1 - Strings/Lab - Strings.playground/Pages/1. Exercise - String Basics.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+/*:
+ ## Exercise - String Basics
+ 
+ Create a `name` constant and assign it a string literal representing your name.
+ */
+let name = "CypherPoet"
+
+/*:
+ Create a `favoriteQuote` constant and assign it the following string literal:
+ 
+ - "My favorite quote is <INSERT QUOTE HERE>."
+ 
+ Write in your own favorite quote where indicated, and be sure to include escaped quotation marks. Print the value of `favoriteQuote`
+ */
+let favoriteQuote = "My favorite quote is \"Art is never finished, only abandoned\", by Leonardo da Vinci."
+
+/*:
+ Write an if-else statement that prints "There's nothing here" if `emptyString` is empty, and "It's not as empty as I thought" otherwise.
+ */
+let emptyString = ""
+
+print(emptyString.isEmpty ? "There's nothing here" : "It's not as empty as I thought")
+//: page 1 of 5  |  [Next: Exercise - Concatenation and Interpolation](@next)
