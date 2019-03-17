8300891e5907a9fe756e115f5daff9268b913e65
CypherPoet <CypherPoet@gmail.com>
2019-03-16 20:41:05 -0400

    touch on some concepts in lab 3

---
+++ 2 - Introduction to UIKit/Labs/3 - Structures/Lab - Structures.playground/Pages/7. Exercise - Computed Properties and Property Observers.xcplaygroundpage/Contents.swift
@@ -0,0 +1,60 @@
+/*:
+ ## Exercise - Computed Properties and Property Observers
+ 
+ The `Rectangle` struct below has two properties, one for width and one for height. Add a computed property that computes the area of the rectangle (i.e. width * height). Create an instance of `Rectangle` and print the `area` property.
+ */
+struct Rectangle {
+    var width: Int
+    var height: Int
+    
+    var area: Int {
+        return width * height
+    }
+}
+/*:
+ In the `Height` struct below, height is represented in both inches and centimeters. However, if `heightInInches` is changed, `heightInCentimeters` should also adjust to match it. Add a `didSet` to each property that will check if the other property is what it should be, and if not, sets the proper value. If you set the value of the other property even though it already has the right value, you will end up with an infinite loop of each property setting the other.
+ 
+ Create an instance of `Height` and then change one of its properties. Print out the other property to ensure that it was adjusted accordingly.
+ */
+struct Height {
+    var heightInInches: Double {
+        didSet {
+            let properCentimeters = heightInInches * 2.54
+            if heightInCentimeters != properCentimeters {
+                heightInCentimeters = properCentimeters
+            }
+        }
+    }
+    
+    var heightInCentimeters: Double {
+        didSet {
+            let properInches = heightInCentimeters / 2.54
+            if heightInInches != properInches {
+                heightInInches = properInches
+            }
+        }
+    }
+    
+    init(heightInInches: Double) {
+        self.heightInInches = heightInInches
+        self.heightInCentimeters = heightInInches * 2.54
+    }
+    
+    init(heightInCentimeters: Double) {
+        self.heightInCentimeters = heightInCentimeters
+        self.heightInInches = heightInCentimeters / 2.54
+    }
+}
+
+
+var buildingHeight = Height(heightInCentimeters: 3030.3)
+
+print(buildingHeight.heightInCentimeters)
+print(buildingHeight.heightInInches)
+
+buildingHeight.heightInInches = 9235
+
+print(buildingHeight.heightInCentimeters)
+print(buildingHeight.heightInInches)
+
+//: [Previous](@previous)  |  page 7 of 10  |  [Next: App Exercise - Mile Times and Congratulations](@next)
