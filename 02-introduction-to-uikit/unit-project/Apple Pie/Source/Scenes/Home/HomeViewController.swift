8bc72d2c167e7a83dc715acfa102e65dc3e475bd
CypherPoet <CypherPoet@gmail.com>
2019-03-21 18:14:45 -0400

    Add button outlets and action connections

--- 02-introduction-to-uikit/unit-project/Apple Pie/Source/Scenes/Home/HomeViewController.swift
+++ 02-introduction-to-uikit/unit-project/Apple Pie/Source/Scenes/Home/HomeViewController.swift
@@ -9,12 +9,43 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var answerSpacesLabel: UILabel!
+    @IBOutlet weak var answerLetterLabel: UILabel!
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var letterButtons: [UIButton]!
+    
+    var answerWord = ""
+    
+    var currentAnswer = "" {
+        didSet {
+            
+        }
+    }
+    
+    var currentScore = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(currentScore)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+}
 
 
+// MARK: - Event handling
+
+extension HomeViewController {
+    @IBAction func letterButtonPressed(_ button: UIButton) {
+        guard let letter = button.titleLabel?.text else {
+            fatalError("Failed to read letter from button")
+        }
+        
+        button.isEnabled = false
+    }
 }
 
