cf82a96642a1f3a7cd368812ca96cd4ff018e1df
CypherPoet <CypherPoet@gmail.com>
2019-04-05 03:07:46 -0400

    Setup UI for question 2

--- 03-navigation-and-workflows/unit-project/MythicalCreatureQuiz/MythicalCreatureQuiz/Scenes/Questions/Question2ViewController.swift
+++ 03-navigation-and-workflows/unit-project/MythicalCreatureQuiz/MythicalCreatureQuiz/Scenes/Questions/Question2ViewController.swift
@@ -9,39 +9,107 @@
 import UIKit
 
 class Question2ViewController: UIViewController {
-    lazy var question: Question = makeQuestion()
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet var answerSwitch: [UISwitch]!
+    @IBOutlet weak var submitButton: UIButton!
     
+    enum SubmissionState {
+        case submitEnabled
+        case submitDisabled
+    }
+    
+    var currentSubmissionState: SubmissionState = .submitDisabled {
+        didSet { submissionStateChanged() }
+    }
+    
+    lazy var question: Question = Question(text: "Members of your species have lived to...")
     weak var delegate: QuestionViewControllerDelegate? = nil
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 
-extension Question2ViewController: QuestionViewController {
-    func makeQuestion() -> Question {
-        return Question(text: "Question 2")
+// MARK: - Computed Properties
+
+extension Question2ViewController {
+    var canSubmitAnswer: Bool {
+        return answerSwitch.first { $0.isOn == true } != nil
+    }
+}
+
+
+// MARK: - Lifecycle
+
+extension Question2ViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        questionLabel.text = question.text
+        submitButton.isEnabled = false
+        submitButton.alpha = 0.5
+    }
+}
+
+
+// MARK: - Event handling
+
+extension Question2ViewController {
+    @IBAction func answerSwitchFlipped(_ sender: UISwitch) {
+        currentSubmissionState = canSubmitAnswer ? .submitEnabled : .submitDisabled
     }
     
+    
+    @IBAction func submitButtonTapped(_ button: UIButton) {
+        // TODO: Handle logic
+        
+        // (All: Assume wizard)
+        //
+        // -10 years (if checked: wizard)
+        //
+        // Lived? (only: vampire)
+        //
+        // 300 years (only: mermaid)
+        //
+        // 1000+ years (only: elf)
+        //
+        // Lived? + 300 years: vampire
+        //
+        // 300 + 1000:
+        //     - is -10 checked: wizard
+        //          - else: elf
+        //
+        // Any other two: wizard
+        
+    }
+
+    
+}
+
+
+// MARK: - Private Helper Methods
+
+extension Question2ViewController {
+    func submissionStateChanged() {
+        switch currentSubmissionState {
+        case .submitDisabled:
+            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+                self?.submitButton.alpha = 0.5
+            })
+        case .submitEnabled:
+            UIView.animate(withDuration: 0.3, animations: { [weak self] in
+                self?.submitButton.alpha = 1.0
+            })
+        }
+    }
+}
+
+
+
+// MARK: - QuestionViewController
+
+extension Question2ViewController: QuestionViewController {
     func handleAnswer(for creature: Creature) {
+        question.answeredFor = creature
+        
         delegate?.answerSubmitted(for: question)
     }
-    
-    
 }
