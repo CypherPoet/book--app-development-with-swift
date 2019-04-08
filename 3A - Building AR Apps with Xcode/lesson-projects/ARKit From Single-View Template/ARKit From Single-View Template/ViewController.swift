48bee2cabf8a64f39e623239243e4183e73b5fa8
CypherPoet <CypherPoet@gmail.com>
2019-04-08 03:13:13 -0400

    add ARKit-Template lesson project

---
+++ 3A - Building AR Apps with Xcode/lesson-projects/ARKit From Single-View Template/ARKit From Single-View Template/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ARKit From Single-View Template
+//
+//  Created by Brian Sipple on 4/8/19.
+//  Copyright © 2019 Brian Sipple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
